f7c3d5259d30a50d3b7949b925e273280a9eea47
Brandon Wallin <braywallin@gmail.com>
2026-06-21 20:33:27 -0400

    Begun creating entry form

---
+++ src/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField, SelectField, DateField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Optional
+class EntryForm(FlaskForm):
+    amount = FloatField('Amount', validators=[DataRequired()])
+    entry_type = SelectField('Income or Expense?', choices=[('income', 'Income'), ('expense', 'Expense')], validators=[DataRequired()])
+    category = SelectField('Category', choices=[('rent', 'Rent'), ('utilities', 'Utilities'), ('gas', 'Gas'), ('school_costs', 'Tuition/Fees'), ('car_insurance', 'Car Insurance'), ('renters_insurance', 'Renter\'s Insurance'), ('phone_bill', 'Phone Bill'), ('groceries', 'Groceries'), ('outside_food', 'Eating Out'), ('entertainment', 'Entertainment')],validators=[DataRequired()])
+    date = DateField('Date', validators=[DataRequired()])
+    description = StringField('Description', validators=[Optional()])
